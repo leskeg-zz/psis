d9d7c104a4b1f2ad9eb72741fd1d5b98661a5f78
leskeg <gabriel.leske@gmail.com>
2015-06-05 15:09:40 +0200

    init

---
+++ mysqlconnection.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from ipdb import set_trace 
+import MySQLdb as mdb
+
+cnx = mdb.connect('127.0.0.1','root','rootREE','dri')
+# cnx = connector.MySQLConnection(user=sys.argv[1], password=sys.argv[2], host='10.231.202.242', database='dri')
+set_trace()
